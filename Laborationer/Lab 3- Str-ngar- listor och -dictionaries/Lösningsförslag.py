de800376d752d78b8e0ec81f487b2534efd9ad2f
darkraven92 <36090181+darkraven92@users.noreply.github.com>
2018-07-09 00:15:37 +0200

    First assignment done
    List

--- Laborationer/Lab 3- Str-ngar- listor och -dictionaries/Lösningsförslag.py
+++ Laborationer/Lab 3- Str-ngar- listor och -dictionaries/Lösningsförslag.py
@@ -1,18 +1,41 @@
-class Dictionary:
-    lists = []
+def menu():
+   wordliststr = []
+   descliststr = []
 
-    def __init__(self, word, description):
-        self.lists = []
-        self.word = word
-        self.description = description
+   while True:
 
-    def fullDesc(self):
-        return '{} {}'.format(self.word, self.description)
+       print(""""
+       1. Insert
+       2. Lookup
+       3. Exit
+       """)
 
-    def addNewWord(self):
-        lists = []
-        self.word = lists.append(self.word)
 
+       ch = input("> ")
+       if ch == "1":
+           addWord = input("Insert word: ")
+           if addWord not in wordliststr:
+               wordliststr.append(addWord)
+               print(wordliststr)
+               addDesc = input("Add description: ")
+               descliststr.append(addDesc)
+           else:
+               print("Word is in list")
+       elif ch == "2":
+           lookupword = input("Word to lookup: ")
+           if lookupword in wordliststr:
+               print("Description for",lookupword,":",descliststr[wordliststr.index(lookupword)])
+           else:
+               print("Word not fund")
+       elif ch == "3":
+           print("Exiting program")
+           break
+       else:
+           print("Enter a vaild input")
+
+
+
+menu()
 
 
 
