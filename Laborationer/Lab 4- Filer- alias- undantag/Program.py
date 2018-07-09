313b6d10bf5247474f0c64a6bef3c1615a01e5c1
darkraven92 <36090181+darkraven92@users.noreply.github.com>
2018-07-09 11:34:40 +0200

    Update

--- Laborationer/Lab 4- Filer- alias- undantag/Program.py
+++ Laborationer/Lab 4- Filer- alias- undantag/Program.py
@@ -1,1 +1,19 @@
 
+
+class telefonbok:
+
+    def __init__(self):
+        self.teledict = {}
+        self.kommandon = {"add":self.add}
+
+    def menu(self):
+        while True:
+            x = input(print("> "))
+            y = x.split()
+
+    def add(self,namn,nummer):
+        pass
+
+
+if __name__ == "__main__":
+    telefonbok().menu()
